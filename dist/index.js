27cd6b54750829b7447c4a6b4a634eae44dbf247
easyno <dlwlsghgk124@naver.com>
2020-01-05 21:45:44 +0900

    study typescript's type and generic function make format function at Date into a different file

--- dist/index.js
+++ dist/index.js
@@ -1,68 +1,36 @@
-const addZero = (value) => {
-    if (typeof value === "string") {
-        return "0" + value;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//제너릭 함수
+function getFirstElem(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error("getFirstElemOrNull: Arguments is not array!");
     }
-    else if (typeof value === "number") {
-        return addZero(value.toString());
+    if (arr.length === 0) {
+        throw new Error("getFristElemOrNull: Arguments is an empty array!");
     }
-};
-Date.prototype.format = function (f, isNumber) {
-    const date = this;
-    const result = f.replace(/(yyyy|MM|DD|dd|hh|mm|ss)/gi, (replaced) => {
-        switch (replaced) {
-            case "yyyy":
-                return date.getFullYear().toString();
-            case "MM":
-                return (date.getMonth() + 1).toString();
-            case "DD":
-                return date.getDate().toString();
-            case "dd":
-                return date.getDay().toString();
-            case "hh":
-                const h = date.getHours();
-                return h < 10 ? addZero(h) : h.toString();
-            case "mm":
-                const m = date.getMinutes();
-                return m < 10 ? addZero(m) : m.toString();
-            case "ss":
-                const s = date.getSeconds();
-                return s < 10 ? addZero(s) : s.toString();
-        }
-    });
-    if (isNumber) {
-        return parseInt(result);
-    }
-    return result;
-};
-const initArray = (size, length) => {
-    const result = [];
-    for (let i = 0; i < length; i++) {
-        result[i] = Array(size).fill(0);
-    }
-    return result;
-};
-const fillThisMonth = (table, startDay, endDate, value, week) => {
-    if (value > endDate) {
-        return table;
-    }
-    table[week][startDay++] = value++;
-    if (startDay === 7) {
-        startDay = 0;
-        week += 1;
-    }
-    return fillThisMonth(table, startDay, endDate, value, week);
-};
-const buildCalendar = (today) => {
-    const tableCalendar = initArray(7, 6);
-    const todayYear = today.getFullYear();
-    const todayMonth = today.getMonth();
-    const thisDate = new Date(todayYear, todayMonth, 1);
-    const lastDate = new Date(todayYear, todayMonth + 1, 0);
-    const prevDate = new Date(todayYear, todayMonth, 0);
-    console.log(thisDate.getDay(), lastDate.getDate());
-    console.log(fillThisMonth(tableCalendar, thisDate.getDay(), lastDate.getDate(), 1, 0));
-    return tableCalendar;
-};
-const date = new Date();
-buildCalendar(date);
+    return arr[0] ? arr[0] : null;
+}
+const languages = ["Typescript", "JavaScript", "React"];
+const language = getFirstElem(languages);
+const numbers = [1, 3, 5, 7, 9];
+const number = getFirstElem(numbers);
+const trueOrFalse = [true, false, true, false];
+const trueValue = getFirstElem(trueOrFalse);
+const persons = [
+    { name: "jinho", age: 24 },
+    { name: "gaon", age: 24 },
+    { name: "hunseop", age: 25 }
+];
+const person = getFirstElem(persons);
+const tuplePersons = [
+    ["lee", 24, "male"],
+    ["lee", 24, "female"],
+    ["song", 25, "male"]
+];
+const tuplePerson = getFirstElem(tuplePersons);
+console.log(tuplePerson);
+console.log(person);
+console.log(trueValue);
+console.log(number);
+console.log(language);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,91 +1,46 @@
-const addZero = (value: any): string => {
-	if (typeof value === "string") {
-		return "0" + value;
-	} else if (typeof value === "number") {
-		return addZero(value.toString());
+import { buildCalendar } from "./Calendar";
+
+//제너릭 함수
+function getFirstElem<T>(arr: T[]): T {
+	if (!Array.isArray(arr)) {
+		throw new Error("getFirstElemOrNull: Arguments is not array!");
 	}
-};
-type formatType = string | number;
-interface Date {
-	format(f: string, isNumber?: boolean): formatType;
+	if (arr.length === 0) {
+		throw new Error("getFristElemOrNull: Arguments is an empty array!");
+	}
+	return arr[0] ? arr[0] : null;
 }
+const languages: string[] = ["Typescript", "JavaScript", "React"];
+const language = getFirstElem<string>(languages);
 
-Date.prototype.format = function(f: string, isNumber?: boolean): formatType {
-	const date = this;
-	const result = f.replace(
-		/(yyyy|MM|DD|dd|hh|mm|ss)/gi,
-		(replaced: string): string => {
-			switch (replaced) {
-				case "yyyy":
-					return date.getFullYear().toString();
-				case "MM":
-					return (date.getMonth() + 1).toString();
-				case "DD":
-					return date.getDate().toString();
-				case "dd":
-					return date.getDay().toString();
-				case "hh":
-					const h = date.getHours();
-					return h < 10 ? addZero(h) : h.toString();
-				case "mm":
-					const m = date.getMinutes();
-					return m < 10 ? addZero(m) : m.toString();
-				case "ss":
-					const s = date.getSeconds();
-					return s < 10 ? addZero(s) : s.toString();
-			}
-		}
-	);
-	if (isNumber) {
-		return parseInt(result);
-	}
-	return result;
-};
+const numbers: number[] = [1, 3, 5, 7, 9];
+const number = getFirstElem<number>(numbers);
 
-const initArray = (size: number, length: number): number[][] => {
-	const result = [];
-	for (let i = 0; i < length; i++) {
-		result[i] = Array(size).fill(0);
-	}
-	return result;
-};
-const fillThisMonth = (
-	table: number[][],
-	startDay: number,
-	endDate: number,
-	value: number,
-	week: number
-): number[][] => {
-	if (value > endDate) {
-		return table;
-	}
-	table[week][startDay++] = value++;
-	if (startDay === 7) {
-		startDay = 0;
-		week += 1;
-	}
-	return fillThisMonth(table, startDay, endDate, value, week);
-};
-const buildCalendar = (today: Date): number[][] => {
-	const tableCalendar = initArray(7, 6);
+const trueOrFalse: boolean[] = [true, false, true, false];
+const trueValue = getFirstElem<boolean>(trueOrFalse);
 
-	const todayYear = today.getFullYear();
-	const todayMonth = today.getMonth();
-	const thisDate = new Date(todayYear, todayMonth, 1);
-	const lastDate = new Date(todayYear, todayMonth + 1, 0);
-	const prevDate = new Date(todayYear, todayMonth, 0);
-	console.log(thisDate.getDay(), lastDate.getDate());
-	console.log(
-		fillThisMonth(
-			tableCalendar,
-			thisDate.getDay(),
-			lastDate.getDate(),
-			1,
-			0
-		)
-	);
-
-	return tableCalendar;
+type Person = {
+	name: string;
+	age: number;
 };
-const date = new Date();
-buildCalendar(date);
+const persons: Person[] = [
+	{ name: "jinho", age: 24 },
+	{ name: "gaon", age: 24 },
+	{ name: "hunseop", age: 25 }
+];
+const person = getFirstElem<Person>(persons);
+
+type TuplePerson = [string, number, string];
+const tuplePersons: TuplePerson[] = [
+	["lee", 24, "male"],
+	["lee", 24, "female"],
+	["song", 25, "male"]
+];
+const tuplePerson = getFirstElem<TuplePerson>(tuplePersons);
+
+
+console.log(tuplePerson);
+console.log(person);
+console.log(trueValue);
+console.log(number);
+console.log(language);
